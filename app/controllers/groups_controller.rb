c6f9a299c09261716c991dd3325ed6d77166d14b
tana1818 <tanakagen1124@gmail.com>
2019-02-11 18:46:02 +0900

    deviseのview作成、controller設定

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,29 @@
 class GroupsController < ApplicationController
+  before_action :set_groups, only: %i[update]
+  def index
+  end
+
+  def new
+    @group = Group.new
+    # @group_user << current_user
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.users << current_user
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => []})
+  end
+
+  def set_groups
+    @group = Group.find(params[:id])
+  end
 end
