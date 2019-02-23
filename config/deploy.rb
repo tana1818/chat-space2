3c7c0118408e0a845a3384f11d49db77b7df66de
tana1818 <tanakagen1124@gmail.com>
2019-02-23 18:42:22 +0900

    Fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,13 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
+set :linked_files, %w{ config/secrets.yml }
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+}
 set :application, 'chat-space2'
 set :repo_url,  'git@github.com:tana1818/chat-space2.git'
 
